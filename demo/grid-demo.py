9e7566ef5e476ce269a5f53d665c6e6e7122a3c6
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-05-17 10:47:40 +0200

    simplify adding functions to vtk writer and test it

--- demo/grid-demo.py
+++ demo/grid-demo.py
@@ -18,11 +18,14 @@ for element in onedgrid.elements:
 m_yaspgrid = grid.get("YaspGrid", dimgrid=2)
 yaspgrid = m_yaspgrid.LeafGrid("../data/unitcube-2d.dgf")
 
+vtk_yaspgrid = yaspgrid.vtkWriter()
+
 def expr_global(x):
     return [-(x[1] - 0.5)*math.sin(x[0]*12)]
 
 ggf = yaspgrid.globalGridFunction("expr_global", expr_global)
 print("ggf:", ggf, " | ", ggf.name, " with dimRange = ", ggf.dimRange)
+ggf.addToVTKWriter(vtk_yaspgrid, vtk_yaspgrid.DataType.PointData)
 for element in yaspgrid.elements:
     lf = ggf.localFunction(element)
     x = [0.5, 0.5]
@@ -34,6 +37,7 @@ def expr_local(element, x):
   return [abs(expr_global(geo.position(x))[0] - expr_global(geo.center)[0])]
 
 lgf = yaspgrid.localGridFunction("expr_local", expr_local)
+lgf.addToVTKWriter(vtk_yaspgrid, vtk_yaspgrid.DataType.CellData)
 print("lgf:", lgf, " | ", lgf.name, " with dimRange = ", lgf.dimRange)
 for element in yaspgrid.elements:
     lf = lgf.localFunction(element)
@@ -42,12 +46,15 @@ for element in yaspgrid.elements:
     print("lgf( ", y, " ) = ", lf.evaluate(x), " | ", expr_local(element,x))
 
 ggf = yaspgrid.globalGridFunction("MathExpression", function.MathExpression(["-(x1-0.5)","x0-1./2."]))
+ggf.addToVTKWriter(vtk_yaspgrid, vtk_yaspgrid.DataType.PointData)
 print("ggf:", ggf, " | ", ggf.name, " with dimRange = ", ggf.dimRange)
 for element in yaspgrid.elements:
     lf = ggf.localFunction(element)
     x = [0.5, 0.5]
     y = element.geometry.position(x)
     print("ggf( ", y, " ) = ", lf.evaluate(x))
+
+vtk_yaspgrid.write("grid_demo");
 
 # ... a 3d alugrid
 #start_time = timeit.default_timer()

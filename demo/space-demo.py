04636e3e36f4ba6fe7cf847310b7a013636fcc5c
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-05-20 14:25:29 +0200

    implement a first interpolate method

--- demo/space-demo.py
+++ demo/space-demo.py
@@ -3,5 +3,17 @@ from __future__ import print_function
 import dune.fem.grid as grid
 import dune.fem.space as space
 
-yaspGrid = grid.leafGrid("../data/unitcube-2d.dgf", "YaspGrid", dimgrid=2)
-lagrangeSpace = space.create("Lagrange", yaspGrid)
+import math
+
+yaspgrid = grid.leafGrid("../data/unitcube-2d.dgf", "YaspGrid", dimgrid=2)
+lagrangespace = space.create("Lagrange", yaspgrid)
+
+def expr_global(x):
+    return [-(x[1] - 0.5)*math.sin(x[0]*12)]
+
+gf = yaspgrid.globalGridFunction("expr_global", expr_global)
+df = lagrangespace.interpolate(gf)
+
+vtk_yaspgrid = yaspgrid.vtkWriter()
+df.addToVTKWriter(vtk_yaspgrid, vtk_yaspgrid.DataType.PointData)
+vtk_yaspgrid.write("space_demo");

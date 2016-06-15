e550a39c57eb7286a0ad99e2bd3e5535dea65332
dedner <a.s.dedner@warwick.ac.uk>
2016-06-15 12:49:45 +0100

    allow to pass a name to the interpolate method - kwargs did not work...

--- demo/space-demo.py
+++ demo/space-demo.py
@@ -10,21 +10,21 @@ import math
 def testSpace(gridtype, **kwargs):
     grid2d = grid.leafGrid("../data/unitcube-2d.dgf", gridtype, dimgrid=2)
     vtk = grid2d.vtkWriter()
-    lagrangespace = space.create("Lagrange", grid2d)
+    lagrangespace = space.create("Lagrange", grid2d, dimrange=2)
 
     def expr_global(x):
-        return [-(x[1] - 0.5)*math.sin(x[0]*12)]
+        return [-(x[1] - 0.5)*math.sin(x[0]*12), 0]
 
     gf = grid2d.globalGridFunction("expr_global", expr_global)
     df = lagrangespace.interpolate(gf)
 
     df.addToVTKWriter(vtk, vtk.PointData)
 
-    df2 = lagrangespace.interpolate([5,3])
+    df2 = lagrangespace.interpolate([5,3], "53" )
     df2.addToVTKWriter(vtk, vtk.CellData)
 
-    df3 = lagrangespace.interpolate(df)
-    df3.addToVTKWriter(vtk, vtk.PointData)
+    df3 = lagrangespace.interpolate(df, "copy" )
+    df3.addToVTKWriter(vtk, vtk.PointData )
 
     vtk.write("space_demo");
 

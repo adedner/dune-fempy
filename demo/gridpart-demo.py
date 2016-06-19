d6861b0d47c922631f1365f3fd80aa85fa0130f3
dedner <a.s.dedner@warwick.ac.uk>
2016-06-19 21:15:55 +0100

    improved the demo a bit

--- demo/gridpart-demo.py
+++ demo/gridpart-demo.py
@@ -8,8 +8,9 @@ import dune.fem.gridpart as gridpart
 def testGridPart(gridtype):
     grid2d = fem.leafGrid("../data/unitcube-2d.dgf", gridtype, dimgrid=2)
 
+    t = 0
     def expr_global(x):
-        return [x[0]*(x[0]+1),(x[0]+1.)*x[1]] # ,math.sin(x[0]*x[1]*2*math.pi)] # problem in vtk with dimensionworld increase in geogp
+        return [x[0]*(x[0]+1),(x[0]+1.)*x[1]*math.sin(0.1+2.*math.pi*t)] # ,math.sin(x[0]*x[1]*2*math.pi)] # problem in vtk with dimensionworld increase in geogp
 
     gf = grid2d.globalGridFunction("expr_global", expr_global)
     df = grid2d.interpolate(gf, space="Lagrange", name="test")
@@ -17,9 +18,15 @@ def testGridPart(gridtype):
     geogp = gridpart.create("Geometry", df )
     vtk = geogp.vtkWriter()
     gfnew = geogp.globalGridFunction("global", expr_global)
-
     gfnew.addToVTKWriter(vtk, vtk.PointData)
-    vtk.write("gridpart_demo");
+
+    dt = 0.01
+    count = 0
+    while t<1:
+        t += dt
+        count += 1
+        df.interpolate(gf)
+        vtk.write("gridpart_demo"+str(count));
 
 print("YASPGRID B")
 testGridPart("YaspGrid")

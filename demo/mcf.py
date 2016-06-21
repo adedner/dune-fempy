e7d36068deed0cb3b91edb5fbf7cf03ac9208a5e
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2016-06-21 17:25:52 +0100

    mcf almost running

--- demo/mcf.py
+++ demo/mcf.py
@@ -15,7 +15,7 @@ import dune.fem.scheme as scheme
 grid2d    = fem.leafGrid("../data/sphere.dgf", "ALUSimplexGrid", dimgrid=2, dimworld=3)
 # discrete function for Gamma(t) and setup surface grid
 print("positions")
-positions = grid2d.interpolate(lambda x: x, space="Lagrange", name="psoitions", variant="global")
+positions = grid2d.interpolate(lambda x: x, space="Lagrange", name="positions", variant="global")
 surface   = gridpart.create("Geometry", positions )
 # vtk writer
 vtk       = surface.vtkWriter()
@@ -23,48 +23,47 @@ vtk       = surface.vtkWriter()
 sp        = space.create( "Lagrange", surface, dimrange=3, polorder=1 )
 # final time and time step
 endTime = 1.
-dt      = 0.01
+dt      = 0.001
 
 
 # set up left and right hand side models
 # --------------------------------------
-model     = duneuflmodel.DuneUFLModel(2,3)
+model     = duneuflmodel.DuneUFLModel(3,3)
 u         = model.trialFunction()
 v         = model.testFunction()
-x         = model.spatialCoordinate()
-tau       = model.coefficient('tau')
-old       = model.coefficient('old')
 
-model.setCoefficient("tau",[dt])   # set time step
-a = ( tau[0]*ufl.inner(ufl.grad(u),ufl.grad(v)) + ufl.inner(u,v) ) * ufl.dx(0)
+a = ( dt*0.5*ufl.inner(ufl.grad(u),ufl.grad(v)) +
+      ufl.inner(u,v) ) * ufl.dx(0)
 model.generate(a,"mcf_left")
-Model = model.makeAndImport(surface)
-mcfModel  = Model.get()
+mcfModel = model.makeAndImport(surface).get()
 
-a = ( ufl.inner(u,v)) * ufl.dx(0)
+a = ( -dt*0.5*ufl.inner(ufl.grad(u),ufl.grad(v)) +
+      ufl.inner(u,v) ) * ufl.dx(0)
 model.generate(a,"mcf_right")
-Model = model.makeAndImport(surface)
-rhsModel  = Model.get()
+rhsModel = model.makeAndImport(surface).get()
 
 # now set up schemes for left and right hand side
 # -----------------------------------------------
 # u^{n+1} and forcing
-solution  = sp.interpolate( lambda x: x, variant="global" )
-forcing   = sp.interpolate( lambda x: x, variant="global" )
+solution  = sp.interpolate( lambda x: x, name="solution", variant="global" )
+forcing   = sp.interpolate( [0,0,0], name="forcing" )
 # left hand side scheme
 solver    = scheme.create( "FemScheme", solution, mcfModel, "mcf" )
 # right hand side scheme
-rhs       = scheme.create( "FemScheme", forcing, rhsModel, "rhs" )
+rhs       = scheme.create( "FemScheme", forcing,  rhsModel, "rhs" )
+solution.addToVTKWriter(vtk, vtk.PointData)
+forcing.addToVTKWriter(vtk, vtk.PointData)
 
 # time lopp
 # ---------
 count   = 0
 t       = 0.
-solution.addToVTKWriter(vtk, vtk.PointData)
 vtk.write("mcf"+str(count));
 
 while t<endTime:
+    forcing.clear()
     rhs(solution,forcing)
+    solution.clear()
     solver.solve(target=solution, rhs=forcing)
     t     += dt
     count += 1

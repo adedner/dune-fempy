da64e1e029686f7c461ebb9bbfbbf70cb41fcda7
dedner <a.s.dedner@warwick.ac.uk>
2016-06-24 11:06:27 +0100

    bug fix

--- demo/mcf.py
+++ demo/mcf.py
@@ -16,12 +16,12 @@ grid2d = fem.leafGrid("../data/sphere.dgf", "ALUSimplexGrid", dimgrid=2, dimworl
 grid2d.hierarchicalGrid.globalRefine(2)
 
 # discrete function for Gamma(t) and setup surface grid
-positions = grid2d.interpolate(lambda x: \
-                x * (1.+0.5*math.sin(2.*math.pi*x[0]*x[1])*math.cos(math.pi*x[2]) ),\
+positions = grid2d.interpolate(lambda x:
+                x * (1.+0.5*math.sin(2.*math.pi*x[0]*x[1])*math.cos(math.pi*x[2])),
             space="Lagrange", name="positions")
 surface   = gridpart.create("Geometry", positions)
 # space for discrete solution on Gamma(t)
-sp        = space.create( "Lagrange", surface, dimrange=3, polorder=1 )
+sp        = space.create( "Lagrange", surface, dimrange=3, polorder=1)
 # final time and time step
 endTime   = 0.25
 dt        = 0.0025
@@ -48,23 +48,23 @@ rhsModel = model.makeAndImport(surface,name="mcf_right").get()
 # now set up schemes for left and right hand side
 # -----------------------------------------------
 # u^{n+1} and forcing
-solution  = sp.interpolate( lambda x: x, name="solution")
-forcing   = sp.interpolate( [0,0,0], name="forcing" )
+solution  = sp.interpolate(lambda x: x, name="solution")
+forcing   = sp.interpolate([0,0,0], name="forcing" )
 # left hand side scheme
-solver    = scheme.create( "FemScheme", solution, mcfModel, "mcf" )
+solver    = scheme.create("FemScheme", solution, mcfModel, "mcf")
 # right hand side scheme
-rhs       = scheme.create( "FemScheme", forcing,  rhsModel, "rhs" )
+rhs       = scheme.create("FemScheme", forcing,  rhsModel, "rhs")
 
 # time lopp
 # ---------
 count   = 0
 t       = 0.
-surface.writeVTK( "mcf", pointdata=[solution], number=count)
+surface.writeVTK("mcf", pointdata=[solution], number=count)
 
 while t<endTime:
-    rhs( solution,forcing )
-    solver.solve( forcing, solution, assemble=(count==0) )
+    rhs(solution, forcing)
+    solver.solve(forcing, solution)
     t     += dt
     count += 1
-    surface.writeVTK( "mcf", pointdata=[solution], number=count)
-    positions.assign( solution.dofVector() )
+    surface.writeVTK("mcf", pointdata=[solution], number=count)
+    positions.assign(solution.dofVector())

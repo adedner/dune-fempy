fc6502ef4104e1c419c31f2ff6444c491ca975bf
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-06-25 15:09:14 +0200

    comment out second example

--- demo/laplace.py
+++ demo/laplace.py
@@ -18,7 +18,7 @@ INTERVAL
 """
 
 grid = fem.leafGrid(dgf, "ALUSimplexGrid", dimgrid=2, refinement="conforming")
-spc = space.create( "Lagrange", grid, dimrange=1, polorder=2)
+spc = space.create("Lagrange", grid, dimrange=1, polorder=2)
 
 # why dimWorld?
 ufl2model = duneuflmodel.DuneUFLModel(grid.dimWorld, 1)
@@ -26,20 +26,20 @@ u = ufl2model.trialFunction()
 v = ufl2model.testFunction()
 x = ufl2model.spatialCoordinate()
 
-a = ( ufl.inner(ufl.grad(u), ufl.grad(v)) + ufl.inner(u,v) ) * ufl.dx(0)
+a = (ufl.inner(ufl.grad(u), ufl.grad(v)) + ufl.inner(u,v)) * ufl.dx(0)
 b = ufl.sin(x[0])*ufl.sin(x[1]) * v[0] * ufl.dx(0)
 ufl2model.generate(a,b)
 model = ufl2model.makeAndImport(grid,name="laplace").get()
 
-laplace = scheme.create("FemScheme", spc, laplaceModel, "laplace")
+laplace = scheme.create("FemScheme", spc, model, "laplace")
 grid.writeVTK("laplace_1", pointdata=[laplace.solve()])
 
-ufl2model.clear()
-
-bnd   = 100.*ufl2model.x0*ufl2model.x1*(1-ufl2model.x0)*(1-ufl2model.x1)
-exact = [sympy.atan(bnd*bnd)]
-ufl2model.generate(a,exact=exact)
-laplaceModel = ufl2model.makeAndImport(grid,name="laplace",exact=exact).get()
-
-laplace = scheme.create("FemScheme", spc, laplaceModel, "laplace")
-grid.writeVTK("laplace_2", pointdata=[laplace.solve()])
+#ufl2model.clear()
+#
+#bnd   = 100.*ufl2model.x0*ufl2model.x1*(1-ufl2model.x0)*(1-ufl2model.x1)
+#exact = [sympy.atan(bnd*bnd)]
+#ufl2model.generate(a,exact=exact)
+#model = ufl2model.makeAndImport(grid,name="laplace",exact=exact).get()
+#
+#laplace = scheme.create("FemScheme", spc, model, "laplace")
+#grid.writeVTK("laplace_2", pointdata=[laplace.solve()])

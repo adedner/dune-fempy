a1bb3db5bc64977c9dacde0277cbddf4beb9c0ab
dedner <a.s.dedner@warwick.ac.uk>
2016-06-25 13:03:54 +0100

    added second laplace problem

--- demo/laplace.py
+++ demo/laplace.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-import math
+import math,sympy
 from mpi4py import MPI
 
 import ufl
@@ -17,20 +17,29 @@ INTERVAL
 #
 """
 
-grid = fem.leafGrid(dgf, "ALUSimplexGrid", dimgrid=2)
+grid = fem.leafGrid(dgf, "ALUSimplexGrid", dimgrid=2, refinement="conforming")
 spc = space.create( "Lagrange", grid, dimrange=1, polorder=2)
 
 # why dimWorld?
-model = duneuflmodel.DuneUFLModel(grid.dimWorld, 1)
-u = model.trialFunction()
-v = model.testFunction()
-x = model.spatialCoordinate()
+ufl2model = duneuflmodel.DuneUFLModel(grid.dimWorld, 1)
+u = ufl2model.trialFunction()
+v = ufl2model.testFunction()
+x = ufl2model.spatialCoordinate()
 
-a = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx(0)
+a = ( ufl.inner(ufl.grad(u), ufl.grad(v)) + ufl.inner(u,v) ) * ufl.dx(0)
 b = ufl.sin(x[0])*ufl.sin(x[1]) * v[0] * ufl.dx(0)
-model.generate(a,b)
-laplaceModel = model.makeAndImport(grid,name="laplace").get()
+ufl2model.generate(a,b)
+model = ufl2model.makeAndImport(grid,name="laplace").get()
 
 laplace = scheme.create("FemScheme", spc, laplaceModel, "laplace")
+grid.writeVTK("laplace_1", pointdata=[laplace.solve()])
 
-grid.writeVTK("laplace", pointdata=[laplace.solve()])
+ufl2model.clear()
+
+bnd   = 100.*ufl2model.x0*ufl2model.x1*(1-ufl2model.x0)*(1-ufl2model.x1)
+exact = [sympy.atan(bnd*bnd)]
+ufl2model.generate(a,exact=exact)
+laplaceModel = ufl2model.makeAndImport(grid,name="laplace",exact=exact).get()
+
+laplace = scheme.create("FemScheme", spc, laplaceModel, "laplace")
+grid.writeVTK("laplace_2", pointdata=[laplace.solve()])

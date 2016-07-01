69ae447162472940d6361abbb2afdeb9331861be
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-07-01 09:18:11 +0200

    towards coefficient support

--- demo/heat.py
+++ demo/heat.py
@@ -1,44 +1,33 @@
 from __future__ import print_function
-import math
+
 from mpi4py import MPI
-import dune.fem as fem
 
-import ufl
-import dune.models.femufl as duneuflmodel
+import math
+from ufl import *
 
-dgf = """DGF
-
-INTERVAL
-0  0
-1  1
-16 16
-#
-"""
-
-deltaT = 0.01
-
-grid = fem.leafGrid(fem.cartesianDomain([0,0],[1,1],[16,16]), "ALUSimplexGrid", dimgrid=2)
-spc = fem.create.space( "Lagrange", grid, dimrange=1, polorder=2)
-
-ufl2model = duneuflmodel.DuneUFLModel(grid.dimWorld, 1, 'Heat')
-u         = ufl2model.trialFunction()
-v         = ufl2model.testFunction()
-u_n       = ufl2model.coefficient('u_n')
+import dune.models.elliptic
+import dune.ufl
+import dune.fem
 
 # Crank Nicholson
 theta = 0.5
-a = (ufl.inner(u-u_n, v) +
-     deltaT * ufl.inner(ufl.grad(theta*u+(1-theta)*u_n), ufl.grad(v))) * ufl.dx(0)
-ufl2model.generate(a)
-heatModel = ufl2model.makeAndImport(grid,name="heat").get()
+deltaT = 0.01
+
+grid = dune.fem.leafGrid(dune.fem.cartesianDomain([0,0],[1,1],[16,16]), "ALUSimplexGrid", dimgrid=2, refinement="conforming")
+spc = dune.fem.create.space("Lagrange", grid, dimrange=1, polorder=2)
+
+uflSpace = dune.ufl.Space(grid.dimGrid, 1, grid.dimWorld)
+u = TrialFunction(uflSpace)
+v = TestFunction(uflSpace)
+u_n = Coefficient(uflSpace)
+
+a = (inner(u - u_n, v) + deltaT * inner(grad(theta*u + (1-theta)*u_n), grad(v))) * dx(0)
+
+model = dune.models.elliptic.importModel("heatmodel", grid, dune.models.elliptic.compileUFL(a == 0)).get()
+scheme = dune.fem.create.scheme("FemScheme", spc, model, "scheme")
 
 solution = spc.interpolate(lambda x: [ math.atan( (10.*x[0]*(1-x[0])*x[1]*(1-x[1]))**2 ) ], name="heat")
-heatScheme = fem.create.scheme("FemScheme", spc, heatModel, "heat")
-
 grid.writeVTK("heat", pointdata=[solution], number=0)
-# vtk = grid.writeVTK("heat", pointdata=[solution], number=0)
-# u_n = spc.interpolate(solution)
-# heatModel.setu_n(u_n)
 
 steps = int(1 / deltaT)
 for n in range(1,steps+1):
@@ -46,6 +35,3 @@ for n in range(1,steps+1):
     heatModel.setu_n(old_solution)
     solution = heatScheme.solve()
     grid.writeVTK("heat", pointdata=[solution], number=n)
-    # u_n.assign(solution)
-    # heatScheme.solve( target=solution )
-    # vtk.write("heat", n)

d739e189dcdfb06617edbec2659f9eb967894385
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-07-02 21:09:09 +0200

    allow disabling of temporary variables in code generation

--- demo/ellipticmodel.py
+++ demo/ellipticmodel.py
@@ -3,7 +3,10 @@ from __future__ import print_function
 import math
 from ufl import *
 
-uflSpace = dune.ufl.Space(2, 1)
+from dune.ufl import Space as UFLSpace
+from dune.models.elliptic import compileUFL, SourceWriter
+
+uflSpace = UFLSpace(2, 1)
 u = TrialFunction(uflSpace)
 v = TestFunction(uflSpace)
 x = SpatialCoordinate(uflSpace.cell())
@@ -11,12 +14,12 @@ x = SpatialCoordinate(uflSpace.cell())
 a = (inner(grad(u), grad(v)) + u[0]*u[0]*v[0]) * dx(0)
 b = sin(2*math.pi*x[0])*sin(2*math.pi*x[1]) * v[0] * dx(0) - x[0]*(1-x[0]) * v[0] * ds(0)
 
-model = dune.models.elliptic.compileUFL(a == b)
+model = compileUFL(a == b, tempVars = False)
 
 # write model to file
 # -------------------
 
-writer = dune.models.elliptic.SourceWriter("mymodel.hh")
+writer = SourceWriter("mymodel.hh")
 writer.openNameSpace('demo')
 model.write(writer, "MyModel")
 writer.closeNameSpace('demo')

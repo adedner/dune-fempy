49d8eb9fd96394f6ed83007cc750b38c86e49781
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2016-07-27 22:48:52 +0100

    added a computation of the radius of the surface to compare with the exact solution in the case of an exact sphere - this used the iterators etc exported by corepy

--- demo/mcf.py
+++ demo/mcf.py
@@ -11,15 +11,19 @@ from dune.models.elliptic import compileUFL, importModel
 from dune.fem import create, leafGrid
 
 order=3
+
 # Basic setup
 # -----------
 # set up reference domain
 grid = leafGrid("../data/sphere.dgf", "ALUSimplexGrid", dimgrid=2, dimworld=3)
-# grid.hierarchicalGrid.globalRefine(2)
+# grid.hierarchicalGrid.globalRefine(1)
 
 # discrete function for Gamma(t) and setup surface grid
-positions = grid.interpolate(lambda x: x * (1.+0.5*math.sin(2.*math.pi*x[0]*x[1])*math.cos(math.pi*x[2])),
+positions = grid.interpolate(\
+              lambda x: x*2.,
+#               lambda x: x * (1.+0.5*math.sin(2.*math.pi*x[0]*x[1])*math.cos(math.pi*x[2])),
             space="Lagrange", name="positions", polorder=order)
+
 surface   = create.gridpart("Geometry", positions)
 # space for discrete solution on Gamma(t)
 spc       = create.space("Lagrange", surface, dimrange=surface.dimWorld, polorder=order)
@@ -43,8 +47,8 @@ rhsModel = dune.models.elliptic.importModel(surface, a_ex == 0).get()
 # now set up schemes for left and right hand side
 # -----------------------------------------------
 # u^{n+1} and forcing
-solution   = spc.interpolate(lambda x: x, name="solution")
-forcing = spc.interpolate([0,]*surface.dimWorld, name="solution")
+solution  = spc.interpolate(lambda x: x, name="solution")
+forcing   = spc.interpolate([0,]*surface.dimWorld, name="solution")
 # left hand side scheme
 solver    = create.scheme("FemScheme", solution, lhsModel, "lhs")
 rhs       = create.scheme("FemScheme", solution, rhsModel, "rhs")
@@ -56,11 +60,30 @@ t       = 0.
 surface.writeVTK("mcf"+str(order)+"-0-", pointdata=[solution], number=count)
 surface.writeVTK("mcf"+str(order)+"-", pointdata=[solution], number=count, subsampling=3)
 
+def calcRadius(surface):
+  # compute R = int_x |x| / int_x 1
+  R   = 0
+  vol = 0
+  for e in surface.elements():
+      rule = grid._module.QuadratureRules(surface.dimension).rule(e.type, 4)
+      for p in rule.points:
+          geo = e.geometry
+          R   += geo.position(p.position).two_norm * geo.volume * p.weight
+          vol += geo.volume * p.weight
+  return R/vol
+
+R0 = calcRadius(surface)
+
 while t < endTime:
     rhs(solution, forcing)
     solver.solve(forcing, solution )
     t     += dt
     count += 1
+    print("time: ", t)
     surface.writeVTK("mcf"+str(order)+"-0-", pointdata=[solution], number=count)
     surface.writeVTK("mcf"+str(order)+"-3-", pointdata=[solution], number=count, subsampling=3)
     positions.assign(solution.dofVector())
+    if 1: # count % 1:
+        R      = calcRadius( surface )
+        Rexact = math.sqrt(R0*R0-4.*t)
+        print("R_h=",R, "Rexact=",Rexact, "difference=",R-Rexact)

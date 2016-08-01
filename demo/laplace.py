5b525509a6e5c58b0db6cc054f12c1e8dffbc7a9
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2016-08-02 00:02:04 +0100

    added ufl.Constants and tackled issue #9

--- demo/laplace.py
+++ demo/laplace.py
@@ -9,19 +9,39 @@ from ufl import *
 from dune.models.elliptic import importModel
 import dune.ufl
 import dune.fem
+import dune.fem.function as gf
+
+# dune.fem.create.spaceGenerator.force = True
+
+dune.femmpi.parameter.append("../data/parameter")
 
 grid = dune.fem.leafGrid(dune.fem.cartesianDomain([0,0],[1,1],[16,16]), "ALUSimplexGrid", dimgrid=2, refinement="conforming")
 spc = dune.fem.create.space("Lagrange", grid, dimrange=1, polorder=2)
 
-uflSpace = dune.ufl.Space((grid.dimGrid, grid.dimWorld), 1)
+uflSpace = dune.ufl.Space((grid.dimGrid, grid.dimWorld), 1, field="double")
 u = TrialFunction(uflSpace)
 v = TestFunction(uflSpace)
+imag = Coefficient(uflSpace)
+const = VectorConstant(triangle,1)
+coeff = Coefficient(uflSpace)
 x = SpatialCoordinate(uflSpace.cell())
 
-f = cos(2*math.pi*x[0])*cos(2*math.pi*x[1])
+f = const[0]*coeff[0]* (cos(2*math.pi*x[0])*cos(2*math.pi*x[1]) + cos(2*math.pi*x[0])*cos(2*math.pi*x[1]))
 
 a = (inner(grad(u), grad(v)) + inner(u,v)) * dx
 b = f * v[0] * dx
 
-scheme = dune.fem.create.scheme("FemScheme", spc, importModel(grid, a == b).get(), "scheme")
-grid.writeVTK("laplace", pointdata=[scheme.solve()])
+model = importModel(grid, a==b).get()
+model.setConstant(const.number, [100.])
+gfunc = gf.MathExpression(["1."])
+coeffFunc = grid.globalGridFunction("global_velocity", gfunc)
+model.setCoefficient(coeff.number, coeffFunc)
+
+scheme = dune.fem.create.scheme("FemScheme", spc, model,\
+        "scheme", storage="Istl",\
+   parameters={"fem.solver.newton.linabstol": 1e-9,
+    "fem.solver.newton.linreduction": 1e-9,
+    "fem.solver.newton.verbose": 1,
+    "fem.solver.newton.linear.verbose": 1})
+
+grid.writeVTK("laplace", pointdata=[ scheme.solve() ])

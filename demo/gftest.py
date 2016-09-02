8f36900ca98f7ccdb40ec6a6537d6742e6336112
lloyd.connellan <l.connellan@warwick.ac.uk>
2016-09-02 13:12:42 +0100

    change to localfunctionadapter

--- demo/gftest.py
+++ demo/gftest.py
@@ -12,7 +12,6 @@ code = """value[0] = sin(x[0]);
     value[1] = cos(x[1]);
 """
 func = gridFunction(grid, code).get()
-print(dir(func))
 
 solution   = spc.interpolate([0,0,0], name="solution")
 solution.interpolate( func )

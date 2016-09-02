5988cfd0c1646229324ec4d10d624d74ecb5a627
lloyd.connellan <l.connellan@warwick.ac.uk>
2016-09-02 16:01:25 +0100

    fix to gridfunction get method

--- demo/gftest.py
+++ demo/gftest.py
@@ -3,13 +3,11 @@ from mpi4py import MPI
 import math
 import dune.fem
 
-#from dune.models.gridfunction import gridFunction
-
 grid = dune.fem.leafGrid(dune.fem.cartesianDomain([0,0],[1,1],[16,16]), "ALUSimplexGrid", dimgrid=2, refinement="conforming")
 spc = dune.fem.create.space("Lagrange", grid, dimrange=2, polorder=2)
 
-code = """value[0] = sin(x[0]);
-    value[1] = cos(x[1]);
+code = """value[0] = sin(xGlobal[0]);
+    value[1] = cos(xGlobal[1]);
 """
 func = grid.localFunction(code)
 

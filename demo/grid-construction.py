1d43bf791518ada30710ca5c79e55f35f6576607
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-09-28 15:52:48 +0200

    adapt grid-construction.py to new create mechanism

--- demo/grid-construction.py
+++ demo/grid-construction.py
@@ -1,9 +1,11 @@
+from __future__ import print_function
+
 import numpy
-from mpi4py import MPI
+
 import dune.common
-import dune.grid
-import dune.alugrid
 import dune.fem
+
+import dune.create as create
 
 from scipy.spatial import Delaunay
 # import matplotlib.pyplot as plt
@@ -25,20 +27,20 @@ triangles = Delaunay(points).simplices
 # plt.plot(points[:,0], points[:,1], 'o')
 # plt.show()
 
-alugrid = dune.grid.create("ALUConform", {'vertex':points, 'simplex':triangles}, dimgrid=2)
+alugrid = create.grid("ALUConform", {'vertex':points, 'simplex':triangles}, dimgrid=2)
 output = alugrid.vtkWriter()
 output.write("grid_construction000")
 
 print("GridFactory 2")
-alugrid = dune.grid.create("ALUConform", {'vertex':points, 'simplex':triangles}, dimgrid=2)
+alugrid = create.grid("ALUConform", {'vertex':points, 'simplex':triangles}, dimgrid=2)
 
 print("from file 1")
-alugrid = dune.grid.create("ALUConform", "../data/unitcube-2d.dgf", dimgrid=2)
+alugrid = create.grid("ALUConform", "../data/unitcube-2d.dgf", dimgrid=2)
 output = alugrid.vtkWriter()
 output.write("grid_construction001")
 
 print("from file 2")
-alugrid = dune.grid.create("ALUConform", (dune.common.reader.dgf,"../data/unitcube-2d.dgf"), dimgrid=2)
+alugrid = create.grid("ALUConform", (dune.common.reader.dgf,"../data/unitcube-2d.dgf"), dimgrid=2)
 output = alugrid.vtkWriter()
 output.write("grid_construction002")
 
@@ -50,9 +52,9 @@ INTERVAL
 16 16
 #
 """
-alugrid = dune.grid.create("ALUConform", dune.grid.string2dgf(dgf), dimgrid=2)
+alugrid = create.grid("ALUConform", dune.grid.string2dgf(dgf), dimgrid=2)
 output = alugrid.vtkWriter()
 output.write("grid_construction003")
 
 print("cartsesian domain")
-alugrid = dune.grid.create("ALUConform", dune.grid.cartesianDomain([0,0],[1,1],[16,16]), dimgrid=2)
+alugrid = create.grid("ALUConform", dune.grid.cartesianDomain([0,0],[1,1],[16,16]), dimgrid=2)

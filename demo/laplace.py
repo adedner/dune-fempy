2f1e665e5e5c12f889566db86c2181d4ae738b3f
dedner <a.s.dedner@warwick.ac.uk>
2016-10-02 12:41:43 +0100

    get laplace demo to work again now using create process for each step

--- demo/laplace.py
+++ demo/laplace.py
@@ -8,17 +8,15 @@ from ufl import *
 import dune.ufl
 import dune.fem
 import dune.fem.function as gf
-import dune.fem.space
-import dune.fem.scheme
 
 import dune.create as create
 
 dune.fem.parameter.append("../data/parameter")
 
-grid = create.grid("SPIsotropic", dune.grid.cartesianDomain([0, 0], [1, 1], [8, 8]), dimgrid=2)
-#grid = create.grid("ALUConform", dune.grid.cartesianDomain([0, 0], [1, 1], [8, 8]), dimgrid=2)
+# grid = create.grid("SPIsotropic", dune.grid.cartesianDomain([0, 0], [1, 1], [8, 8]), dimgrid=2)
+grid = create.grid("ALUConform", dune.grid.cartesianDomain([0, 0], [1, 1], [8, 8]), dimgrid=2)
 # spc  = dune.fem.create.space("DGONB", grid, dimrange=1, order=2)
-spc  = dune.fem.space.create("Lagrange", grid, dimrange=1, order=2)
+spc  = dune.create.space("Lagrange", grid, dimrange=1, order=2)
 
 uflSpace = dune.ufl.Space((grid.dimGrid, grid.dimWorld), 1, field="double")
 u = TrialFunction(uflSpace)
@@ -33,8 +31,8 @@ a = a + 20./(u[0]*u[0]+1.) * v[0] * dx
 model = dune.fem.create.ellipticModel( grid, a==0,
         exact=exact, dirichlet={ 1:exact } ) ()
 
-# scheme = dune.fem.create.scheme("DGFemScheme", spc, model,\
-scheme = dune.fem.scheme.create("H1", spc, model, "scheme",\
+# scheme = create.scheme("DGFemScheme", spc, model,\
+scheme = create.scheme("h1", spc, model, "scheme",\
        parameters=\
        {"fem.solver.newton.linabstol": 1e-10,
         "fem.solver.newton.linreduction": 1e-10,
@@ -42,16 +40,15 @@ scheme = dune.fem.scheme.create("H1", spc, model, "scheme",\
         "fem.solver.newton.linear.verbose": 0},\
         storage="istl")
 
-#exact_gf = grid.function("exact", 5, ufl=exact)
+exact_gf = create.function("ufl", grid, "exact", 5, exact)
 for i in range(2):
     print("solve on level",i)
     uh = scheme.solve()
     def l2error(en,x):
         val = uh.localFunction(en).evaluate(x) - exact_gf.localFunction(en).evaluate(x)
         return [ val[0]*val[0] ];
-    #l2error_gf = grid.function( "error", 5, localExpr=l2error )
-    #error = math.sqrt( grid.l2Norm(l2error_gf) )
-    #print("size:",grid.size(0),"L2-error:",error)
-    #grid.writeVTK("laplace", pointdata=[ uh,l2error_gf ])
-    grid.writeVTK("laplace", pointdata=[uh])
-    #grid.hierarchicalGrid.globalRefine(2)
+    l2error_gf = create.function("local", grid, "error", 5, l2error )
+    error = math.sqrt( l2error_gf.integrate()[0] )
+    print("size:",grid.size(0),"L2-error:",error)
+    grid.writeVTK("laplace", pointdata=[ uh,l2error_gf ])
+    # grid.hierarchicalGrid.globalRefine(2)

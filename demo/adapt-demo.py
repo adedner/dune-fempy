f8acb5a4ed90d113b9d6f0d22038e953dbd923dd
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-10-04 17:00:01 +0200

    make adapt-demo run, but disable adaption for now

--- demo/adapt-demo.py
+++ demo/adapt-demo.py
@@ -3,23 +3,28 @@ from __future__ import print_function
 import math
 
 import dune.common as common
-from dune.alugrid import aluConformGrid
-import dune.fem as fem
 
-grid = aluConformGrid("../data/unitcube-2d.dgf", dimgrid=2)
+from dune.grid import cartesianDomain
+from dune.alugrid import aluConformGrid
+from dune.fem.function import levelFunction, partitionFunction
+from dune.fem.space import lagrange
+from dune.fem.view import adaptiveLeafGridView
+
+domain = cartesianDomain([0, 0], [1, 1], [8, 8])
+grid = adaptiveLeafGridView(aluConformGrid(domain, dimgrid=2))
 
 # interpolate some data onto macro grid
-phi = grid.interpolate(lambda x: [math.sin(math.pi*x[0])*math.cos(math.pi*x[1])], space="Lagrange", name="phi", order=1)
+spc = lagrange(grid, dimrange=1, order=1)
+phi = spc.interpolate(lambda x: [math.sin(math.pi*x[0])*math.cos(math.pi*x[1])], name="phi")
 
 # add phi to vtk output
-t = 0
-grid.writeVTK( "initial", pointdata=[phi] )
+grid.writeVTK("initial", pointdata=[phi])
 
 maxLevel = 8
 hgrid = grid.hierarchicalGrid
 marker = common.Marker
 
-def mark(element):
+def mark(element, t):
     y = element.geometry.center - [0.5+0.2*math.cos(t), 0.5+0.2*math.sin(t)]
     if y.two_norm2 < 0.2*0.2 and y.two_norm2 > 0.1*0.1:
       return marker.refine if element.level < maxLevel else marker.keep
@@ -27,17 +32,17 @@ def mark(element):
       return marker.coarsen
 
 for i in range(0,maxLevel):
-    hgrid.mark(mark)
-    hgrid.adapt([phi])
-    hgrid.loadBalance([phi])
+    hgrid.mark(lambda e: mark(e, 0))
+    #hgrid.adapt([phi])
+    #hgrid.loadBalance([phi])
 
 nr = 0
-while t < 2*math.pi:
-    print('time:',t)
-    hgrid.mark(mark)
-    hgrid.adapt([phi])
-    hgrid.loadBalance([phi])
-    grid.writeVTK("adapt", pointdata=[phi], celldata=[grid.levelFunction(), grid.partitionFunction()], number=nr)
-    print(common.comm.rank, "size: ", grid.size(0))
-    t = t+0.1
-    nr = nr+1
+while 0.1*nr < 2*math.pi:
+    if common.comm.rank == 0:
+        print('time:', 0.1*nr)
+    hgrid.mark(lambda e: mark(e, 0.1*nr))
+    #hgrid.adapt([phi])
+    #hgrid.loadBalance([phi])
+    grid.writeVTK("adapt", pointdata=[phi], celldata=[levelFunction(grid), partitionFunction(grid)], number=nr)
+    print("[" + str(common.comm.rank), "] Size: " + str(grid.size(0)))
+    nr += 1

e0d6ea8427f20ae9fcaf7bb80d94d63f06e18c64
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-10-17 17:33:20 +0200

    make DG scheme work
    Note: There still seems to be a minor bug in the derivative, as the scheme
    requires 2 Newton iterations instead of one.

--- demo/heat.py
+++ demo/heat.py
@@ -12,6 +12,12 @@ import dune.create as create
 theta = 0.5
 deltaT = 0.01
 
+import dune.fem
+dune.fem.parameter.append({"fem.verboserank": 0,
+                           "istl.preconditioning.method": "ilu-0",
+                           "istl.preconditioning.iterations": 1,
+                           "istl.preconditioning.relaxation": 1.2})
+
 def compute():
     # set up a 2d simplex grid over the interval [0,1]^2 with h = 1/16
     grid = create.grid("ALUConform", cartesianDomain([0,0],[1,1],[16,16]), dimgrid=2)
@@ -20,7 +26,7 @@ def compute():
     spc = create.space("DGONB", grid, dimrange=1, order=2)
 
     # set up initial conditions
-    solution = spc.interpolate(lambda x: [math.atan((10.0 * x[0] * (1-x[0]) * x[1] * (1-x[1]))**2)], name="u")
+    solution = spc.interpolate(lambda x: [math.atan((10.0 * x[0] * (1-x[0]) * x[1] * (1-x[1]))**2)], name="u", storage="istl")
     grid.writeVTK("heat", pointdata=[solution], number=0)
 
     # get a discrete function to hold the old solution and tell the model to use that for the coefficient u_n
@@ -39,13 +45,14 @@ def compute():
     model = create.model("elliptic", grid, a == 0, coefficients={u_n:old_solution})
 
     # setup structure for olver parameters
-    solverParameter={"fem.solver.newton.linabstol": 1e-10,
-                     "fem.solver.newton.linreduction": 1e-10,
-                     "fem.solver.newton.verbose": 1,
-                     "fem.solver.newton.linear.verbose": 1}
+    solverParameter={"fem.solver.newton.linabstol": 1e-8,
+                     "fem.solver.newton.linreduction": 1e-8,
+                     "fem.solver.newton.tolerance": 1e-7,
+                     "fem.solver.newton.verbose": "true",
+                     "fem.solver.newton.linear.verbose": "false"}
     # create the solver using a standard fem scheme
-    # scheme = create.scheme("h1galerkin", spc, model, parameters=solverParameter)
-    scheme = create.scheme("dggalerkin", spc, model, 10, parameters=solverParameter)
+    # scheme = create.scheme("h1galerkin", spc, model, parameters=solverParameter, storage="istl")
+    scheme = create.scheme("dggalerkin", spc, model, 5, parameters=solverParameter, storage="istl")
 
     # now loop through time and output the solution after each time step
     steps = int(1 / deltaT)

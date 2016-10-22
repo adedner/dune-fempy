a096fc93a7ae322713d283b8da39c8e59d1f7d17
dedner <a.s.dedner@warwick.ac.uk>
2016-10-22 17:59:55 +0100

    added a scheme and improved the agglomeration - use voronoi cells

--- demo/agglomerated-demo.py
+++ demo/agglomerated-demo.py
@@ -3,6 +3,9 @@
 from __future__ import print_function
 
 import math
+import numpy as np
+from scipy.spatial import Voronoi, voronoi_plot_2d, cKDTree
+
 from ufl import *
 
 import dune.ufl
@@ -30,13 +33,49 @@ def plot(grid, solution):
     except ImportError:
         pass
 
-def compute():
-    grid = create.view("adaptive", grid="ALUConform", constructor=dune.grid.cartesianDomain([0, 0], [1, 1], [8, 8]), dimgrid=2)
-    # spc  = create.space("DGONB", grid, dimrange=1, order=1)
-    spc  = create.space("AgglomeratedDG", grid, lambda en: 0, dimrange=1, order=1)
-    expr_global = lambda x: [-(x[1] - 0.5)*math.sin(x[0]*12)]*dimRange
 
-    gf = create.function("discrete", spc, "expr_global", 1, expr_global)
-    grid.writeVTK("test", pointdata=[gf])
+def compute(grid,NX,NY):
+    voronoi_points = np.random.rand(NX*NY, 2)
+    voronoi_kdtree = cKDTree(voronoi_points)
+    vor = Voronoi(voronoi_points)
+    voronoi_plot_2d(vor).savefig("voronoi"+str(NX*NY)+".pdf", bbox_inches='tight')
+    ind = set()
+    def agglomerate(en):
+        p = en.geometry.center
+        if 0: # Cartesian
+            x = p[0]
+            y = p[1]
+            nx = int(x * NX)
+            ny = int(y * NY)
+            index = nx*NY+ny
+        else:  # Voronoi
+            test_point_dist, test_point_regions = voronoi_kdtree.query([p], k=1)
+            index = test_point_regions[0]
+        ind.add(index)
+        return index
+    spc  = create.space("AgglomeratedDG", grid, agglomerate, dimrange=1, order=3, storage="istl")
+    assert len(ind)==NX*NY, "missing or too many indices provided by agglomoration object. Should be "+str(NX*NY)+" was "+str(len(ind))
 
-compute()
+    uflSpace = dune.ufl.Space((grid.dimGrid, grid.dimWorld), 1, field="double")
+    u = TrialFunction(uflSpace)
+    v = TestFunction(uflSpace)
+    x = SpatialCoordinate(uflSpace.cell())
+    exact = as_vector( [cos(2.*pi*x[0])*cos(2.*pi*x[1])] )
+    a = (inner(grad(u), grad(v)) + inner(u,v)) * dx
+    a = a + 20./(u[0]*u[0]+1.) * v[0] * dx
+    model = create.model("elliptic", grid, a==0, exact=exact, dirichlet={ 1:exact } )
+
+    scheme = create.scheme("dggalerkin", spc, model, 10)
+    df = scheme.solve()
+
+    df_coeff = dune.ufl.GridCoefficient(df)
+    l2error_gf = create.function("ufl", grid, "error", 5,
+            as_vector([(exact[0]-df_coeff[0])**2]) )
+    error = math.sqrt( l2error_gf.integrate() )
+    print("size:",spc.size,"L2-error:", error)
+    grid.writeVTK("test",
+        celldata=[ df, create.function("local",grid,"cells",1,lambda en,x: [agglomerate(en)]) ])
+
+grid = create.view("adaptive", grid="ALUConform", constructor=dune.grid.cartesianDomain([0, 0], [1, 1], [100, 100]), dimgrid=2)
+compute(grid,6,6)
+compute(grid,12,12)

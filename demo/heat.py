c9ee7b2cefd35cfa20dbafbed514600c05194c1b
Andreas Dedner <a.s.dedner@warwick.ac.uk>
2016-10-25 17:48:26 +0100

    use h1 scheme by default since it is still 2.4 compatible

--- demo/heat.py
+++ demo/heat.py
@@ -52,9 +52,9 @@ def compute():
                      "fem.solver.newton.verbose": "true",
                      "fem.solver.newton.linear.verbose": "false"}
     # create the solver using a standard fem scheme
-    # scheme = create.scheme("h1", spc, model, parameters=solverParameter)
+    scheme = create.scheme("h1", spc, model, parameters=solverParameter)
     # scheme = create.scheme("h1galerkin", spc, model, parameters=solverParameter)
-    scheme = create.scheme("dggalerkin", spc, model, 15*theta*deltaT, parameters=solverParameter)
+    # scheme = create.scheme("dggalerkin", spc, model, 15*theta*deltaT, parameters=solverParameter)
 
     # scheme = create.scheme("linearized", scheme, parameters=solverParameter)
     # scheme = create.scheme("linearized", scheme="h1", ubar=solution, space=spc, model=model, parameters=solverParameter)

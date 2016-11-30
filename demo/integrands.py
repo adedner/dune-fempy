b656754718d85474ee42400efe6c5538f0e22b3c
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-11-30 17:07:22 +0100

    make integrands work with coefficients

--- demo/integrands.py
+++ demo/integrands.py
@@ -6,7 +6,7 @@ from ufl import *
 from dune.ufl import Space as UFLSpace
 from dune.source import SourceWriter
 from dune.source.cplusplus import NameSpace
-from dune.models.integrands import compileUFL
+from dune.models.integrands import compileUFL, create
 
 uflSpace = UFLSpace(2, 1)
 u = TrialFunction(uflSpace)
@@ -41,5 +41,4 @@ from dune.models.integrands import load
 domain = cartesianDomain([0, 0], [1, 1], [8, 8])
 grid = aluConformGrid(domain, dimgrid=2)
 
-module = load(grid, integrands)
-integrands = module.create()
+integrands = create(grid, integrands)

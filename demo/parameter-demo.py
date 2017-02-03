24afc8930fb1f4aa051f17d3d31dda6673ea16bb
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-02-03 15:07:03 +0100

    show operator[]

--- demo/parameter-demo.py
+++ demo/parameter-demo.py
@@ -4,5 +4,7 @@ parameter.append( "../data/parameter" )
 parameter.append( {"hallo": 12, "wie": 20, "gehts": "gut?" } )
 # print( str(parameter) )
 parameter.append( "hallo", 11. )
+parameter["quark"] = 12
 parameter.append( "mir", "auch" )
+print(parameter["mir"])
 parameter.append( "../data/parameter" )

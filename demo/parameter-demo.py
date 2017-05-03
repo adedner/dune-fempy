5809fede8f9e1adcd8d8ae6546f06c3928681275
dedner <a.s.dedner@warwick.ac.uk>
2017-05-03 22:19:38 +0100

    bug fix in exporting the parameter class

--- demo/parameter-demo.py
+++ demo/parameter-demo.py
@@ -1,4 +1,4 @@
-from mpi4py import MPI
+from __future__ import print_function
 from dune.fem import parameter
 parameter.append( "../data/parameter" )
 parameter.append( {"hallo": 12, "wie": 20, "gehts": "gut?" } )
@@ -8,3 +8,9 @@ parameter["quark"] = 12
 parameter.append( "mir", "auch" )
 print(parameter["mir"])
 parameter.append( "../data/parameter" )
+
+# test that these changes actually make it through to the C++ side
+from dune.generator import algorithm
+print( algorithm.run('run', 'parameter-demo.hh', unicode("mir"), parameter["mir"]) )
+print( algorithm.run('run', 'parameter-demo.hh', "quark", 12 ) )
+print( "not",algorithm.run('run', 'parameter-demo.hh', "quark", -int( parameter["quark"] )) )

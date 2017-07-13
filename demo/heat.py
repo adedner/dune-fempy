1768f328d509cea41536c3d0c2660b8161bf9886
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-07-13 13:16:53 +0200

    [bugfix] avoid dune.ufl.GridCoefficient

--- demo/heat.py
+++ demo/heat.py
@@ -4,7 +4,7 @@ import math
 from ufl import *
 
 from dune.grid import cartesianDomain
-from dune.ufl import GridCoefficient, Space
+from dune.ufl import Space
 
 import dune.create as create
 
@@ -30,14 +30,13 @@ def compute():
     grid.writeVTK("heat", pointdata=[solution], number=0)
 
     # get a discrete function to hold the old solution and tell the model to use that for the coefficient u_n
-    old_solution = solution.copy();
+    u_n = solution.copy();
 
     # now define the actual pde to solve:
     #            u - u_n deltaT laplace( theta u + (1-theta) u_n ) = 0
     uflSpace = Space((grid.dimGrid, grid.dimWorld), 1)
     u = TrialFunction(uflSpace)
     v = TestFunction(uflSpace)
-    u_n = GridCoefficient(solution)
     tau = Constant(triangle)
     a = (inner(u - u_n, v) + tau * inner(grad(theta*u + (1-theta)*u_n), grad(v))) * dx
 
@@ -46,7 +45,7 @@ def compute():
     #model.setConstant(tau,[deltaT])
 
     model = create.model("integrands", grid, a == 0)
-    model.setCoefficient(u_n, old_solution)
+    model.setCoefficient(u_n, u_n)
     model.setConstant(tau, [deltaT])
 
     # setup structure for olver parameters
@@ -68,7 +67,7 @@ def compute():
     # now loop through time and output the solution after each time step
     steps = int(0.1 / deltaT)
     for n in range(1,steps+1):
-        old_solution.assign(solution)
+        u_n.assign(solution)
         scheme.solve(target=solution)
         grid.writeVTK("heat", pointdata=[solution], number=n)
 

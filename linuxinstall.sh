fb30ebb5799d29a21d0495068baf1e6399a6e057
dedner <a.s.dedner@warwick.ac.uk>
2017-10-19 18:03:45 +0100

    improve the install script

--- linuxinstall.sh
+++ linuxinstall.sh
@@ -9,10 +9,17 @@ if ! which cmake >/dev/null; then
     exit 1
 fi
 
+DUNE_VERSION='master'
+OPTFLAGS='-std=c++11 -Wall -Winit-self -Wfatal-errors -DNDEBUG -O3 -funroll-loops -ffast-math \
+  -fexcess-precision=fast -march=native \
+  -msse -mfpmath=sse --param inline-unit-growth=700'
+
 echo 'This script is for installing dune-fempy on Linux
 
 It will install DUNE with the following components:
-dune-common dune-geometry dune-grid dune-istl dune-localfunctions dune-alugrid dune-fem dune-corepy dune-fempy'
+dune-common dune-geometry dune-grid dune-istl dune-localfunctions dune-alugrid dune-fem dune-corepy dune-fempy
+Using version $DUNE_VERSION
+'
 
 read -p "Continue (y/n)? " choice
 case "$choice" in y|Y|yes|Yes )
@@ -22,20 +29,20 @@ case "$choice" in y|Y|yes|Yes )
           folder_name='dune'
         fi
         echo "Making directory: $folder_name"
-        mkdir $HOME/$folder_name
+        mkdir $folder_name
         read -p "Install DUNE (make sure cmake is installed first) (y/n)? " choice4
         case "$choice4" in
           y|Y|yes|Yes )
             cd $HOME/$folder_name
-            git clone -b releases/2.5 https://gitlab.dune-project.org/core/dune-common.git dune-common
-            git clone -b releases/2.5 https://gitlab.dune-project.org/core/dune-geometry.git dune-geometry
-            git clone -b releases/2.5 https://gitlab.dune-project.org/core/dune-grid.git dune-grid
-            git clone -b releases/2.5 https://gitlab.dune-project.org/core/dune-istl.git dune-istl
-            git clone -b releases/2.5 https://gitlab.dune-project.org/core/dune-localfunctions.git dune-localfunctions
-            git clone https://gitlab.dune-project.org/extensions/dune-alugrid.git dune-alugrid
-            git clone -b releases/2.5 https://gitlab.dune-project.org/dune-fem/dune-fem.git dune-fem
-            git clone https://gitlab.dune-project.org/staging/dune-corepy.git dune-corepy
-            git clone ssh://git@gitlab.dune-project.org:22022/dune-fem/dune-fempy.git dune-fempy
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/core/dune-common.git dune-common
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/core/dune-geometry.git dune-geometry
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/core/dune-grid.git dune-grid
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/core/dune-istl.git dune-istl
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/core/dune-localfunctions.git dune-localfunctions
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/extensions/dune-alugrid.git dune-alugrid
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/dune-fem/dune-fem.git dune-fem
+            git clone -b $DUNE_VERSION https://gitlab.dune-project.org/staging/dune-corepy.git dune-corepy
+            git clone -b $DUNE_VERSION ssh://git@gitlab.dune-project.org:22022/dune-fem/dune-fempy.git dune-fempy
             echo 'CMAKE_FLAGS=" -DCMAKE_CXX_FLAGS='\''$OPTFLAGS'\'' \
                   -DDUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS=TRUE \
                   -DALLOW_CXXFLAGS_OVERWRITE=ON \
@@ -43,9 +50,19 @@ case "$choice" in y|Y|yes|Yes )
                   -DCMAKE_DISABLE_FIND_PACKAGE_LATEX=TRUE \
                   -DCMAKE_DISABLE_DOCUMENTATION=TRUE \
                   -DBUILD_SHARED_LIBS=TRUE \
+                  -DADDITIONAL_PIP_PARAMS="-upgrade" \
+                  -DDUNE_PYTHON_INSTALL_LOCATION="user" \
                   " ' > config.opts
             echo 'Installing DUNE'
             ./dune-common/bin/dunecontrol --opts=config.opts all
+            echo 'Setting up python environment'
+            ./dune-corepy/bin/setup-dunepy --opts=config.opts install
+            echo "Completed installation."
+            echo "Demos can be found in $folder_name/dune-fempy/demo,"
+            echo "jupyter notebooks in $folder_name/notebooks."
+            echo "You will need (at least) the python packages ufl,numpy,matplotlib."
+            echo "You can try:"
+            echo "  pip install --upgrade --user ufl numpy matplotlib"
             ;;
           n|N|no|No ) ;;
           * ) echo "Please choose y or n"

a144d5b2af6099125f9d6ba96db03af27e0238f6
dedner <a.s.dedner@warwick.ac.uk>
2017-10-21 15:18:33 +0100

    adapt to renaming of dune-corepy

--- navierstokes/burgers.cc
+++ navierstokes/burgers.cc
@@ -1,7 +1,7 @@
 #include <config.h>
 
-// dune-corepy
-#include <dune/corepy/pybind11/pybind11.h>
+// dune-python
+#include <dune/python/pybind11/pybind11.h>
 
 // dune-fempy
 #include <dune/fempy/py/grid/function.hh>

cbc28e3ce8aeecb2fce3f6ea8bd43c9949042dcd
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2017-11-02 11:55:38 +0100

    [docker] use the releases/2.6 branch when building Docker container

--- docker/install-dune.sh
+++ docker/install-dune.sh
@@ -9,17 +9,17 @@ export CMAKE_FLAGS=" \
   -DDUNE_GRID_EXPERIMENTAL_GRID_EXTENSIONS=TRUE \
   -DDUNE_PYTHON_INSTALL_LOCATION=system \
 "
-wget -qO - https://gitlab.dune-project.org/core/dune-common/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/core/dune-geometry/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/core/dune-grid/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/core/dune-istl/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/core/dune-localfunctions/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/extensions/dune-alugrid/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/extensions/dune-spgrid/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/staging/dune-uggrid/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/staging/dune-python/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/dune-fem/dune-fem/repository/archive.tar.gz?ref=master | tar xz
-wget -qO - https://gitlab.dune-project.org/dune-fem/dune-fempy/repository/archive.tar.gz?ref=master | tar xz
+wget -qO - https://gitlab.dune-project.org/core/dune-common/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/core/dune-geometry/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/core/dune-grid/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/core/dune-istl/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/core/dune-localfunctions/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/extensions/dune-alugrid/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/extensions/dune-spgrid/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/staging/dune-uggrid/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/staging/dune-python/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/dune-fem/dune-fem/repository/archive.tar.gz?ref=releases/2.6 | tar xz
+wget -qO - https://gitlab.dune-project.org/dune-fem/dune-fempy/repository/archive.tar.gz?ref=releases/2.6 | tar xz
 
 patch -d dune-common-* -p 1 < $(dirname $0)/documentation.patch
 

f034f14cd5a1bd982756c9bc20ebab4050c671bd
dedner <a.s.dedner@warwick.ac.uk>
2018-07-25 12:36:33 +0100

    fixed a but with the weights in galerkin operator. Also imporoved the testoperator script to test also non linear operators are applied and assembled correctly

--- test/testoperator.py
+++ test/testoperator.py
@@ -4,32 +4,67 @@ from dune.fem import parameter
 from dune.fem.function import integrate
 import dune.create as create
 from ufl import TestFunction, TrialFunction, SpatialCoordinate, triangle, exp,\
-                dx, grad, inner, as_vector, replace, sqrt
+                dx, grad, inner, as_vector, replace, sqrt, dot,\
+                derivative, action
+from ufl.algorithms.apply_derivatives import apply_derivatives
 from dune.ufl import NamedConstant
 
 parameter.append({"fem.verboserank": -1})
 
 grid = structuredGrid([0, 0], [1, 1], [40, 40])
 
-order     = 1
-dimR      = 1
-quadOrder = 2*order+3
+order     = 2
+dimR      = 2
 spaceName = "lagrange"
 space = create.space(spaceName, grid, dimrange=dimR, order=order)
 
 arg   = space.interpolate(as_vector([1,]*dimR), name='arg')
 destA = space.interpolate(as_vector([0,]*dimR), name='destA')
 destB = space.interpolate(as_vector([0,]*dimR), name='destB')
+destC = space.interpolate(as_vector([0,]*dimR), name='destC')
+destD = space.interpolate(as_vector([0,]*dimR), name='destD')
+destE = space.interpolate(as_vector([0,]*dimR), name='destE')
 
 u  = TrialFunction(space)
 v  = TestFunction(space)
-a  = ( inner(u, v) + inner(grad(u), grad(v)) ) * dx
-a  = inner(u, v) * dx
-op = create.operator("galerkin", a, space)
+x = SpatialCoordinate(space.cell())
 
-A = op.assemble(arg)
-op(arg,destA)
-A(arg,destB)
-# grid.writeVTK('optest', pointdata=[destA,destB])
+ubar = space.interpolate(as_vector([dot(x,x),]*dimR),name="ubar")
+a    = ( inner(0.5*dot(u,u), v[0]) +
+         inner(u[0]*grad(u), grad(v)) ) * dx
+op   = create.operator("galerkin", a, space)
+A    = op.assemble(ubar)
+A(arg,destA)
+
+da   = apply_derivatives(derivative(action(a, ubar), ubar, u))
+dop  = create.operator("galerkin", da, space)
+dop(arg,destB)
 err = integrate(grid, (destA-destB)**2, 5)
-print("error=",err)
+# print("error=",err)
+assert(err.two_norm < 1e-15)
+
+A   = dop.assemble(arg)
+A(arg,destC)
+err = integrate(grid, (destA-destC)**2, 5)
+# print("error=",err)
+assert(err.two_norm < 1e-15)
+
+###############################################################
+
+op(ubar,destA)
+
+lina  = ( inner(0.5*dot(ubar,u), v[0]) +
+          inner(ubar[0]*grad(u), grad(v)) ) * dx
+linop = create.operator("galerkin", lina, space)
+linop(ubar,destD)
+err = integrate(grid, (destA-destD)**2, 5)
+# print("error=",err)
+assert(err.two_norm < 1e-15)
+
+A   = linop.assemble(arg)
+A(ubar,destE)
+err = integrate(grid, (destA-destE)**2, 5)
+# print("error=",err)
+assert(err.two_norm < 1e-15)
+
+# grid.writeVTK('optest', pointdata=[destA,destB,destC,destD,destE])

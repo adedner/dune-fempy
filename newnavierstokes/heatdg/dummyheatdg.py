b284d2ffb7b58cc05de45a3ef73041fdc17a0c48
Birane Kane <you@example.com>
2018-09-25 14:03:39 +0200

    dummy example for Bnd Conditions

--- newnavierstokes/heatdg/dummyheatdg.py
+++ newnavierstokes/heatdg/dummyheatdg.py
@@ -8,6 +8,8 @@ from dune.ufl import NamedConstant
 
 import dune.create as create
 
+from dune.ufl import DirichletBC, Space
+
 # Crank Nicholson
 theta = 0.5
 deltaT = 0.01
@@ -25,11 +27,11 @@ def compute():
     grid = create.grid("ALUConform", "../../data/channelcontraction.dgf", dimgrid=2)
 
     # set up a lagrange scalar space with polynomial order 2 over that grid
-    spc = create.space("Lagrange", grid, dimrange=2, order=2, storage="istl")
+    spc = create.space("dgonb", grid, dimrange=2, order=2, storage="istl")
     # spc = create.space("dgonb", grid, dimrange=1, order=2)
 
     # set up initial conditions
-    solution = spc.interpolate(lambda x: [math.atan((10.0 * x[0] * (1-x[0]) * x[1] * (1-x[1]))**2)], name="u")
+    solution = spc.interpolate( [0,0], name="u")
     vtk = grid.sequencedVTK("heat-dg", pointdata=[solution])
     vtk()
 
@@ -42,19 +44,19 @@ def compute():
 
     u = TrialFunction(uflSpace)
     v = TestFunction(uflSpace)
-    x = SpatialCoordinate(spc)
-    n = FacetNormal(spc)
+    x = SpatialCoordinate(uflSpace.cell())
+    n = FacetNormal(uflSpace.cell())
     mu = 7.5 * 16
     tau = NamedConstant(spc,"tau")
     a  = inner(grad(u), grad(v)) * dx
-    # a -= (inner(outer(jump(u), n('+')), avg(grad(v))) + inner(avg(grad(u)), outer(jump(v), n('+')))) * dS
-    # a += mu * inner(jump(u), jump(v)) * dS
-    # a -= (inner(outer(u, n), grad(v)) + inner(grad(u), outer(v, n))) * ds
-    # a += mu * inner(u, v) * ds
+    a -= tau * (inner(outer(jump(u), n('+')), avg(grad(v))) + inner(avg(grad(u)), outer(jump(v), n('+')))) * dS
+    a += tau * mu * inner(jump(u), jump(v)) * dS
+    a -= tau * (inner(outer(u, n), grad(v)) + inner(grad(u), outer(v, n))) * ds
+    a += tau * mu * inner(u, v) * ds
     a = inner(u - old_solution, v) * dx + tau*theta*a + tau*(1-theta)*replace(a,{u:old_solution})
 
 
-    model = create.model("elliptic", grid, a == 0,
+    model = create.model("integrands", grid, a == 0,
             DirichletBC(uflSpace, [None,None], 2),       # bottom
             DirichletBC(uflSpace, [None,None], 3),      # top
             DirichletBC(uflSpace, [1,0], 4),          # left
@@ -65,10 +67,10 @@ def compute():
     solverParameter={"fem.solver.newton.linabstol": 1e-13,
                      "fem.solver.newton.linreduction": 1e-13,
                      "fem.solver.newton.tolerance": 1e-12,
-                     "fem.solver.newton.verbose": "false",
+                     "fem.solver.newton.verbose": "true",
                      "fem.solver.newton.linear.verbose": "false"}
     # create the solver using a standard fem scheme
-    scheme = create.scheme("h1", model, spc, parameters=solverParameter)
+    scheme = create.scheme("galerkin", model, spc, parameters=solverParameter)
     scheme.model.tau = deltaT
 
     # now loop through time and output the solution after each time step

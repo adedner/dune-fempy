29f2a2c400ebba97c269fb26edba43bedebb61d3
dedner <a.s.dedner@warwick.ac.uk>
2018-09-28 15:05:03 +0100

    improve usage of grid functions

--- test/test-quad.py
+++ test/test-quad.py
@@ -9,6 +9,8 @@ yaspView = yaspGrid(domain)
 
 x = ufl.SpatialCoordinate( ufl.triangle )
 function = ufl.as_vector([ ufl.cos(2*ufl.pi/(0.3+x[0]*x[1])) ])
+# function = dune.create.function("global",gridview=yaspView, name="gl",order=5,
+#            value=lambda x: [math.cos(2*math.pi/(0.3+x[0]*x[1]))] )
 space = dune.create.space("lagrange", yaspView, order=1)
 uh = space.interpolate( function, name="uh" )
 error = dune.create.function("ufl",gridview=yaspView,name="error",order=5,ufl=uh-function,

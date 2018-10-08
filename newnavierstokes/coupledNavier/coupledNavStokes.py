bef882ba789fa3ed5e31185ad34953a96e36e3a9
dedner <a.s.dedner@warwick.ac.uk>
2018-10-08 13:05:30 +0100

    tried to fix the coupledNS example but the iteration produces NAN.

--- newnavierstokes/coupledNavier/coupledNavStokes.py
+++ newnavierstokes/coupledNavier/coupledNavStokes.py
@@ -7,12 +7,12 @@ from dune.grid import cartesianDomain
 
 import dune.create as create
 
-from dune.ufl import DirichletBC, Space
+from dune.ufl import DirichletBC, NamedConstant
 
 
 # Crank Nicholson
 # theta = 1.
-deltaT = 0.001
+deltaT = 1e-5 # 0.001
 viscosity = 1.0e0
 import dune.fem
 dune.fem.parameter.append({"fem.verboserank": 0,
@@ -26,91 +26,66 @@ def compute():
     grid = create.grid("ALUConform", "../../data/channelcontraction.dgf", dimgrid=2)
     grid.hierarchicalGrid.globalRefine(6)
     # set up a lagrange scalar space with polynomial order 2 over that grid
-    spc = create.space("lagrange", grid, dimrange=2, order=2, storage="istl")
-    # spcP = create.space("lagrange", grid, dimrange=1, order=2)
-    spcP = create.space("lagrange", grid, dimrange=1, order=1, storage="istl")
+    spc = create.space("lagrange", grid, dimrange=3, order=2, storage="istl")
 
     # set up initial conditions
 
-    velocity = spc.interpolate( [0,0], name="velocity")
-    pressure = spcP.interpolate( lambda x: [0], name = "pressure", storage = "Istl" )
+    solution = spc.interpolate( [0,0,0], name="velocity")
+    velocity = as_vector([solution[0],solution[1]])
+    pressure = as_vector([solution[2]])
 
-    solution = velocity, pressure
-
-    vtk = grid.sequencedVTK("coupledNavstokes", pointvector={"velocity":velocity})
+    vtk = grid.sequencedVTK("coupledNavstokes",
+            pointvector={"velocity":velocity,"pressure":pressure})
     vtk()
 
     # get a discrete function to hold the old solution and tell the model to use that for the coefficient u_n
     # old_solution = solution.copy()
-    old_velo = velocity.copy()
-
-    # old_solution.name = "uOld"
-    old_velo.name = "uOld"
+    old_sol = solution.copy(name="uOld")
 
     # now define the actual pde to solve:
     #            u - u_n deltaT laplace( theta u + (1-theta) u_n ) = 0
-    uflSpace = Space((grid.dimGrid, grid.dimWorld), 2)
-    uflSpaceP = Space((grid.dimGrid, grid.dimWorld), 2)
 
-    u = TrialFunction(uflSpace)
-    v = TestFunction(uflSpace)
-    p = TrialFunction(uflSpaceP)
-    q = TestFunction(uflSpaceP)
-    x = SpatialCoordinate(uflSpace.cell())
-    n = FacetNormal(uflSpace.cell())
-    mu = 7.5 * 16
+    trial = TrialFunction(spc)
+    test  = TestFunction(spc)
+    u = as_vector([trial[0],trial[1]])
+    v = as_vector([test[0],test[1]])
+    p = as_vector([trial[2]])
+    q = as_vector([test[2]])
+    x = SpatialCoordinate(spc)
 
     dirichlet = conditional(abs(x[0])<1e-8,1.,0.)
 
-    u_n = old_velo
-    tau = Constant(triangle)
-    a = (inner(u - u_n, v) + tau * viscosity * inner(grad(u), grad(v))) * dx
-    a += -1*tau*inner(p[0],div(v)) * dx
-    a += tau* inner(grad(u), outer(v, u)) * dx
+    tau = NamedConstant(spc,name="tau")
+    a = (inner(u - as_vector([old_sol[0],old_sol[1]]), v) +
+         tau * viscosity * inner(grad(u), grad(v))) * dx
+    a -= tau * inner(p[0],div(v)) * dx
+    a += tau * inner(grad(u), outer(v, u)) * dx
     a += inner(div(u), q[0]) * dx
 
-
-    # a -= (inner(outer(jump(u), n('+')), avg(grad(v))) + inner(avg(grad(u)), outer(jump(v), n('+')))) * dS
-    # a += mu * inner(jump(u), jump(v)) * dS
-    # a -= (inner(outer(u, n), grad(v)) + inner(grad(u), outer(v, n))) * ds
-    # a += mu * inner(u, v) * ds
-    # now generate the model code and compile
-    #model = create.model("elliptic", grid, a == 0, coefficients={u_n:old_solution})
-    #model.setConstant(tau,[deltaT])
-
-    # model = create.model("integrands", grid, a == 0)
     model = create.model("elliptic", grid, a == 0,
-            DirichletBC(uflSpace, [None,None], 2),       # bottom
-            DirichletBC(uflSpace, [None,None], 3),      # top
-            DirichletBC(uflSpace, [1,0], 4),          # left
-            DirichletBC(uflSpace, [0,0], 1))                # right
+            DirichletBC(spc, [None,None,None], 2),       # bottom/top
+            DirichletBC(spc, [None,None,None], 3),       # bottom/top
+            DirichletBC(spc, [1,0,None], 4),             # left
+            DirichletBC(spc, [0,0,None], 1))             # right
 
-    model.setConstant(tau, deltaT)
+    model.tau = deltaT
 
     # setup structure for olver parameters
     solverParameter={"fem.solver.newton.linabstol": 1e-13,
                      "fem.solver.newton.linreduction": 1e-13,
                      "fem.solver.newton.tolerance": 1e-12,
                      "fem.solver.newton.verbose": "true",
-                     "fem.solver.newton.linear.verbose": "false"}
+                     "fem.solver.newton.linear.verbose": "true"}
     # create the solver using a standard fem scheme
-    # scheme = create.scheme("h1", spc, model, parameters=solverParameter)
-    # scheme = create.scheme("h1galerkin", spc, model, parameters=solverParameter)
-    # scheme = create.scheme("dggalerkin", spc, model, 15*theta*deltaT, parameters=solverParameter)
-
     scheme = create.scheme("h1", model, spc, parameters=solverParameter)
-
-    # scheme = create.scheme("linearized", scheme, parameters=solverParameter)
-    # scheme = create.scheme("linearized", scheme="h1", ubar=solution, space=spc, model=model, parameters=solverParameter)
 
     endTime = 0.05
     timeStep = deltaT
     time = timeStep
     while time < endTime:
         print( "Time is:", time )
-        old_velo.assign(velocity)
-
-        scheme.solve(target=velocity)
+        old_sol.assign(solution)
+        scheme.solve(target=solution)
         vtk()
         time += timeStep
 

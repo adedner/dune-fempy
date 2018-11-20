3e6cec1f8b31be0e7567912e5828233d6dd0fa5b
kanebe <birane.kane@mathematik.uni-stuttgart.de>
2018-11-20 16:04:05 +0100

    set up test program for fractional step method to solve NS - not working yet

--- newnavier/nsfracstep1.py
+++ newnavier/nsfracstep1.py
@@ -1,13 +1,13 @@
 import dune.create as create
 from dune.grid import cartesianDomain
-from ufl import SpatialCoordinate, CellVolume, TrialFunction, TestFunction,\
+from ufl import SpatialCoordinate, CellVolume, TrialFunction, TestFunction, replace,\
                 inner, outer, dot,triangle, div, grad, dx, as_vector, transpose, Identity
 from dune.ufl import NamedConstant, DirichletBC
 import dune.fem
 from math import pi,log10
 from dune.fem.function import integrate
 
-from ufl import cos, sin, exp, as_vector, dx, grad, inner,sqrt
+from ufl import cos, sin, exp, sqrt
 
 from dune.fem import parameter
 
@@ -16,6 +16,8 @@ from stokesclass import Stokes
 
 parameter.append({"fem.verboserank": 0, "istl.preconditioning.method": "ilu", "istl.preconditioning.iterations": 1, "istl.preconditioning.relaxation": 1.2})
 
+Re      = 100.
+deltaT  = 0.001
 endTime = 0.1
 order   = 2
 
@@ -23,44 +25,74 @@ grid = create.grid("ALUCube",constructor=cartesianDomain([-1,-1],[1,1],[50,50]))
 spcU = create.space("lagrange", grid, dimrange=grid.dimension, order=order, storage="istl")
 spcP = create.space("lagrange", grid, dimrange=1, order=order-1, storage="istl")
 x     = SpatialCoordinate(spcU)
-dt    = NamedConstant(spcU, "dt")
 time  = NamedConstant(spcU, "t")     # current time
 
-gamma_t = lambda t: exp(-2.*pi*pi*mu*t)
-exact_u = as_vector( [-1*cos(1.*pi*x[0])*sin(1.*pi*x[1])*gamma_t(time), sin(1.*pi*x[0])*cos(1.*pi*x[1])*exp(-2.*pi*pi*mu*time)])
-exact_p = as_vector( [ -0.25*(cos(2.*pi*x[0])+cos(2.*pi*x[1]))*exp(-4.*pi*pi*mu*time) ] )
+nu    = 1./Re
+gamma_t = lambda t: exp(-2.*pi*pi*nu*t)
+exact_u = as_vector( [ -1*cos(1.*pi*x[0])*sin(1.*pi*x[1])*gamma_t(time), sin(1.*pi*x[0])*cos(1.*pi*x[1])*exp(-2.*pi*pi*nu*time) ] )
+exact_p = as_vector( [ -0.25*(cos(2.*pi*x[0])+cos(2.*pi*x[1]))*exp(-4.*pi*pi*nu*time) ] )
 
-exact_uend = as_vector( [-1*cos(1.*pi*x[0])*sin(1.*pi*x[1])*exp(-2.*pi*pi*mu*0.1), sin(1.*pi*x[0])*cos(1.*pi*x[1])*gamma_t(endTime)
+exact = lambda t: [replace(exact_u,{time:t}),replace(exact_p,{time:t})]
 
-f           = as_vector( [2.*pi*pi*mu*cos(1.*pi*x[0])*sin(1.*pi*x[1])*gamma_t(time) , -2.*pi*pi*mu*sin(1.*pi*x[0])*cos(1.*pi*x[1])*gamma_t(time) ] )
-f          += as_vector( [0.25*2.*pi*sin(2.*pi*x[0])*exp(-4.*pi*pi*mu*time),  0.25*2.*pi*sin(2.*pi*x[1])*exp(-4.*pi*pi*mu*time)] )
-f          -= as_vector( [mu*2*gamma_t(time)*pi*pi*cos(1.*pi*x[0])*sin(1.*pi*x[1]), mu*-2*gamma_t(time)*pi*pi*sin(1.*pi*x[0])*cos(1.*pi*x[1])] )
+f           = as_vector( [2.*pi*pi*nu*cos(1.*pi*x[0])*sin(1.*pi*x[1])*gamma_t(time) , -2.*pi*pi*nu*sin(1.*pi*x[0])*cos(1.*pi*x[1])*gamma_t(time) ] )
+f          += as_vector( [0.25*2.*pi*sin(2.*pi*x[0])*exp(-4.*pi*pi*nu*time),  0.25*2.*pi*sin(2.*pi*x[1])*exp(-4.*pi*pi*nu*time)] )
+f          -= as_vector( [nu*2*gamma_t(time)*pi*pi*cos(1.*pi*x[0])*sin(1.*pi*x[1]), nu*-2*gamma_t(time)*pi*pi*sin(1.*pi*x[0])*cos(1.*pi*x[1])] )
 f          += as_vector( [cos(1.*pi*x[0])*sin(1.*pi*x[0])*(cos(1.*pi*x[1])*cos(1.*pi*x[1])-sin(1.*pi*x[1])*sin(1.*pi*x[1])),cos(1.*pi*x[1])*sin(1.*pi*x[1])*(cos(1.*pi*x[0])*cos(1.*pi*x[0])-sin(1.*pi*x[0])*sin(1.*pi*x[0]))] )
-f          += nu*exact_u
 
+bcs = [DirichletBC(spcU,exact_u,1)]
 ##############################################
 
+# TODO: set ufl time constant in expression
+solU = spcU.interpolate(exact(0)[0],"velocity")
+solP = spcP.interpolate(exact(0)[1],"pressure")
+spc  = [spcU,spcP]
+solution = [solU,solP]
+
+class PR:
+    def __init__(self):
+        self.stokes  = Stokes(spc,f,bcs,Re,withBurgers=True)
+        self.burgers = Burgers(spc,f,bcs,Re)
+    def solve(self,target):
+        print( 'Solve step 1 - Stokes' )
+        self.stokes.prepare(simTime+0.5*deltaT,2./deltaT,1/2,target)
+        self.stokes.solve(target)
+        print( 'Solve step 2 - Burgers' )
+        self.burgers.prepare(simTime+deltaT,2./deltaT,1/2,target)
+        self.burgers.solve(target)
+class FractionalTheta:
+    def __init__(self):
+        self.stokes  = Stokes(spc,f,bcs,Re,withBurgers=True)
+        self.burgers = Burgers(spc,f,bcs,Re)
+        self.theta = 1-sqrt(2.)/2.
+        self.alpha = 0.5
+        self.beta  = 1-self.alpha
+    def solve(self,target):
+        print( 'Solve step 1 - Stokes' )
+        self.stokes.prepare(simTime+self.theta*deltaT,1./(self.theta*deltaT),self.alpha,target)
+        self.stokes.solve(target)
+        print( 'Solve step 2 - Burgers' )
+        self.burgers.prepare(simTime+deltaT*(1-self.theta),1/((1.-2*self.theta)*deltaT),self.alpha,target)
+        self.burgers.solve(target)
+        print( 'Solve step 3 - Stokes' )
+        self.stokes.prepare(simTime+deltaT,1./(self.theta*deltaT),self.alpha,target)
+        self.stokes.solve(target)
+
+vtk = grid.sequencedVTK("visc1navierfracstep_model2",
+        pointdata={"pressure":solution[1], "exact_p":exact(endTime)[1]},
+        pointvector={"velocity":solution[0],"exact_u":exact(endTime)[0]})
+vtk()
+
+scheme = PR() # FractionalTheta()
 endTime = 0.1
 timeStep = deltaT
-time = timeStep
+simTime = 0
 counter = 0
-while time < endTime:
-    print( "Time is:", time )
-    print( 'Solve step 1 - Stokes' )
-    solveStokes()
-    # print( 'Solve step 2 - Burgers' )
-    solveBurger()
-    # print( 'Solve step 3 - Stokes' )
-    solveStokes()
+while simTime < endTime:
+    print( "Time is:", simTime )
+    scheme.solve(target=solution)
+    simTime += timeStep
 
-    l2error_fn = dot(velocity - exact_uend, velocity - exact_uend)
+    l2error_fn = dot(solU - exact(simTime)[0], solU - exact(simTime)[0])
     l2error = sqrt( integrate(grid, l2error_fn, 5)[0] )
     print('|u_h - u| =', l2error)
-
-
     vtk()
-
-
-    time += timeStep
-
-compute()
--- newnavier/stokesclass.py
+++ newnavier/stokesclass.py
@@ -1,73 +1,86 @@
 from dune import create
 from dune.fem.function import integrate
-
+from ufl import SpatialCoordinate, CellVolume, TrialFunction, TestFunction,\
+                inner, outer, dot,triangle, div, grad, dx, as_vector, transpose, Identity
+from dune.ufl import NamedConstant
 
 class Stokes:
-    def __init__(self,spc,forcing):
+    def __init__(self,spc,forcing,bcs,Re,withBurgers=False):
         spcU = spc[0]
         spcP = spc[1]
+        # TODO add dimDomain property to space
+        dimension = spcU.grid.dimension
         u  = TrialFunction(spcU)
         v  = TestFunction(spcU)
         p  = TrialFunction(spcP)
         q  = TestFunction(spcP)
         mu = NamedConstant(spcU, "mu")
         nu = NamedConstant(spcU, "nu")
-        self.oldVelo = spcU.interpolate([0,]*spcU.dimRange,name="oldU")
-        mainModel    = (mu*dot(u,v) + nu*inner(grad(u)+grad(u).T, grad(v))) * dx
-        gradModel    = -1*inner( p[0]*Identity(grid.dimension), grad(v) ) * dx
+        mainModel    = (mu*dot(u,v) + nu/Re*inner(grad(u)+grad(u).T, grad(v))) * dx
+        gradModel    = -1*inner( p[0]*Identity(dimension), grad(v) ) * dx
         divModel     = -1*inner(div(u),q[0]) * dx
         massModel    = inner(p,q) * dx
         preconModel  = inner(grad(p),grad(q)) * dx
-        f = (dot(forcing,v) +
-             mu*dot(self.oldVelo,v) - nu*inner(grad(self.oldVelo)+grad(self.oldVelo).T, grad(v)) -
+        if forcing is not None:
+            f = dot(forcing,v) * dx
+        else:
+            f = 0
+        if withBurgers:
+             self.oldVelo = spcU.interpolate([0,]*spcU.dimRange,name="oldU")
+             f += (mu*dot(self.oldVelo,v) - (1-nu)/Re*inner(grad(self.oldVelo)+grad(self.oldVelo).T, grad(v)) -
              inner(grad(self.oldVelo), outer(v, self.oldVelo))) * dx
 
-        self.mainOp   = create.scheme("galerkin",spcU,(mainModel==f,DirichletBC(spcU,exact_u,1)), solver="cg")
+        # TODO give better diagonostics when no equation is passed to scheme
+        # TODO galerkin doesn't work here
+        self.mainOp   = create.scheme("h1",spcU,[mainModel==f,*bcs], solver="cg")
         self.gradOp   = create.operator("galerkin",gradModel)
         self.divOp    = create.operator("galerkin",divModel)
         self.massOp   = create.scheme("galerkin",massModel==0)
         self.preconOp = create.scheme("galerkin",preconModel==0)
 
-        self.rhsVelo  = spcU.interpolate([0,0],name="rhsU") # TODO: extract correct dimension
+        self.rhsVelo  = spcU.interpolate([0,]*spcU.dimRange,name="rhsU")
         self.rhsPress = spcP.interpolate([0],name="rhsP")
-        self.r        = rhsPress.copy()
-        self.d        = rhsPress.copy()
-        self.precon   = rhsPress.copy()
-        self.xi       = rhsVelo.copy()
+        self.r        = self.rhsPress.copy()
+        self.d        = self.rhsPress.copy()
+        self.precon   = self.rhsPress.copy()
+        self.xi       = self.rhsVelo.copy()
 
-    def prepare(self,t,mu,nu,velo):
-        try:
-            self.mainOp.model.time = t
-        except: pass
-        self.mainOp.model.mu = mu
-        self.mainOp.model.nu = nu
-        self.oldVelo.assign(velo)
-        self.A = self.mainOp.assemble(self.rhsVelo)
         self.G = self.gradOp.assemble(self.rhsPress)
         self.D = self.divOp.assemble(self.rhsVelo)
         self.M = self.massOp.assemble(self.rhsPress)
         self.P = self.preconOp.assemble(self.rhsPress)
-        solver = {"krylovmethod":"cg","fem.solver.verbose":0}
-        self.Ainv   = self.mainOp.inverseLinearOperator(A,1e-10,parameters=solver)
-        self.Minv   = self.massOp.inverseLinearOperator(M,1e-10,solver)
-        self.Pinv   = self.preconOp.inverseLinearOperator(P,1e-10,solver)
+        self.solver = {"krylovmethod":"cg","fem.solver.verbose":0}
+        self.Minv   = self.massOp.inverseLinearOperator(self.M,1e-10,self.solver)
+        self.Pinv   = self.preconOp.inverseLinearOperator(self.P,1e-10,self.solver)
+
+    def prepare(self,time=0,mu=0,nu=1,oldSolution=None):
+        try:
+            self.mainOp.model.time = time
+        except: pass
+        self.mainOp.model.mu = mu
+        self.mainOp.model.nu = nu
+        if oldSolution:
+            self.oldVelo.assign(oldSolution[0])
+        self.A = self.mainOp.assemble(self.rhsVelo)
+        self.Ainv   = self.mainOp.inverseLinearOperator(self.A,1e-10,parameters=self.solver)
 
     def solve(self,target):
         velocity = target[0]
         pressure = target[1]
         self.mainOp(velocity,self.rhsVelo)                  # assembleRHS ( mainModel_, mainModel_.rightHandSide(), mainModel_.neumanBoundary(), rhsU_ );
         self.rhsVelo *= -1
-        self.G(pressure,xi)                            # gradOperator_(pressure_, xi_);
-        self.rhsVelo -= xi                             # rhs_ -= xi_;
-        self.mainOp.setConstraints(self.rhsVelo)            # mainOperator_.prepare( velocity_, rhsU_ );
-
-        self.Ainv(self.rhsVelo, self.velocity)                   # invMainOp( rhsU_, velocity_ );
+        self.G(pressure,self.xi)                            # gradOperator_(pressure_, xi_);
+        self.rhsVelo -= self.xi                             # rhs_ -= xi_;
+        try:
+            self.mainOp.setConstraints(self.rhsVelo)        # mainOperator_.prepare( velocity_, rhsU_ );
+        except: pass
+        self.Ainv(self.rhsVelo, velocity)                   # invMainOp( rhsU_, velocity_ );
         self.D(velocity,self.rhsPress)                      # divLinearOperator_( velocity_, rhsP_ );
         self.Minv(self.rhsPress, self.r)                         # invMassOp( rhsP_, r_ );
 
         # steps = int(0.1 / deltaT)
         # for n in range(1,10):
-        if self.mainOp.model.mu > 0:                   # if (usePrecond_ && nu_>0.)
+        if False: # self.mainOp.model.mu > 0:                   # if (usePrecond_ && nu_>0.)
             self.precon.clear()                        #   precond_.clear();
             self.Pinv(self.rhsPress, self.precon)                #   invPrecondOp( rhsP_, precond_ );
             self.r *= self.mainOp.model.nu                  #   r_ *= mu_;
@@ -79,8 +92,10 @@ class Stokes:
         for m in range(100):                      # for (int m=0;m<100;++m)
             self.xi.clear()                            #   xi_.clear();
             self.G(self.d,self.rhsVelo)                          #   gradLinearOperator_(d_, rhsU_);
-            self.mainOp.setConstraints(\
-                [0,]*grid.dimension, self.rhsVelo)     #   mainOperator_.prepare( mainModel_.zeroVelocity(), rhsU_ );
+            try:
+                self.mainOp.setConstraints(\
+                    [0,]*grid.dimension, self.rhsVelo)     #   mainOperator_.prepare( mainModel_.zeroVelocity(), rhsU_ );
+            except: pass
             self.Ainv(self.rhsVelo, self.xi)                     #   invMainOp( rhsU_, xi_ );
             self.D(self.xi,self.rhsPress)                        #   divLinearOperator_( xi_, rhsP_ );
             self.rho = delta /\
@@ -89,7 +104,7 @@ class Stokes:
             velocity.axpy(-1*self.rho,self.xi)                #   velocity_.axpy(-rho,xi_);
             self.D(velocity, self.rhsPress)                 #   divLinearOperator_( velocity_, rhsP_ );
             self.Minv(self.rhsPress,self.r)                      #   invMassOp( rhsP_, r_ );
-            if self.mainOp.model.mu > 0:               #   if (usePrecond_ && nu_>0.)
+            if False: # self.mainOp.model.mu > 0:               #   if (usePrecond_ && nu_>0.)
                 self.precon.clear()                    #     precond_.clear();
                 self.Pinv(self.rhsPress,self.precon)             #     invPrecondOp( rhsP_, precond_ );
                 self.r *= self.mainOp.model.nu              #     r_ *= mu_;
@@ -100,4 +115,4 @@ class Stokes:
             if delta < 1e-14: break               #     if ( delta < solverEps_*10. ) break;
             gamma = delta/oldDelta                #     double gamma = delta/oldDelta;
             self.d *= gamma                            #     d_ *= gamma;
-            self.d += r                                #     d_ += r_;
+            self.d += self.r                                #     d_ += r_;

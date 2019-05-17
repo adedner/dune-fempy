4629499a1d17b5fbfa6766f2965912351aab7f21
dedner <a.s.dedner@warwick.ac.uk>
2019-05-17 16:50:54 +0100

    improve gl math rendering

--- gitlab-formatting.py
+++ gitlab-formatting.py
@@ -7,34 +7,25 @@ parser.add_argument('filename', type=str, help='enter tex file to add gitlab mat
 args = parser.parse_args()
 filename = args.filename
 
+mathEnvs = ["gather","eqnarray","align","equation"]
+
 count = 0
 input = open(filename, 'r')
 output = open('output', 'w')
 lines = input.readlines()
+
 for i in range(len(lines)):
     # remove pre-existing code highlighting
     lines[i] = lines[i].replace('[language=Python]', '')
     # add pythonstyle highlighting if not already done
-    if lines[i].startswith('\begin{equation*}'):
-        output.write("```math")
-    elif lines[i].startswith('\begin{equation}'):
-        output.write("```math")
-    elif lines[i].startswith('\begin{align*}'):
-        output.write("```math")
-    elif lines[i].startswith('\begin{align}'):
-        output.write("```math")
-    elif lines[i].startswith('\end{equation*}'):
-        output.write("```")
-    elif lines[i].startswith('\end{equation}'):
-        output.write("```")
-    elif lines[i].startswith('\end{align*}'):
-        output.write("```")
-    elif lines[i].startswith('\end{align}'):
-        output.write("```")
+    if any("\\begin{"+me in lines[i] for me in mathEnvs):
+        output.write("```math\n")
+    elif any("\\end{"+me in lines[i] for me in mathEnvs):
+        output.write("```\n")
     else:
         counter = itertools.count(1)
         output.write( re.sub('\$',
-              lambda m: "$'" if next(counter)%2==1 else "'$",
+              lambda m: "$`" if next(counter)%2==1 else "`$",
               lines[i])
             )
 os.rename("output", filename)

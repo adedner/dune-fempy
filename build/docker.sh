43efe14819dbc3e9d8a6ad2f66ddfd23bef776a1
dedner <a.s.dedner@warwick.ac.uk>
2019-05-17 11:44:39 +0100

    remove the 'docker/vagrant' build instruction - moved to dune-fem-dev repo also added a test_assembly.py script testing assembly for different block sizes

--- build/docker.sh
+++
@@ -1,7 +0,0 @@
-docker build\
-  --build-arg userId=$(id -u) --build-arg groupId=$(id -g)\
-  -t dune .
-
-docker run -it -v $HOST:/host -v dune:/dune\
-  -v /tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=:0\
-  dune bash
